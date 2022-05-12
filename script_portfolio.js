2afd2dd8aad0de6361ba25967aced0e0e9c7eb5d
Antoine Sillard <a.sillard3@gmail.com>
2022-05-12 11:45:48 +0200

    ajout Accueil + Veille

--- script_portfolio.js
+++ script_portfolio.js
@@ -12,35 +12,3 @@ backdrop.addEventListener('click', () => nav.classList.remove('open-nav'));
 trigger.addEventListener('click', () => container.classList.add('nav_is_open'));
 // Supprimer la classe open-nav quand l'utilisateur clique autre part
 backdrop.addEventListener('click', () => container.classList.remove('nav_is_open'));
-
-var textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml11 .line',
-    scaleY: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700
-  })
-  .add({
-    targets: '.ml11 .line',
-    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: 100
-  }).add({
-    targets: '.ml11 .letter',
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=775',
-    delay: (el, i) => 34 * (i+1)
-  }).add({
-    targets: '.ml11',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
