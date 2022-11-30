e4d2c6a6b68f3761bc8136a2ebb58618da233a6d
NeoDark75 <a.sillard3@gmail.com>
2022-11-30 03:03:32 +0100

    Contact/Stage(test)/Slide/NewFont

--- script_portfolio.js
+++ script_portfolio.js
@@ -1,8 +1,11 @@
+// Declaration de constante 
 const trigger = document.querySelector(".trigger");
 const nav = document.querySelector(".full-screen-nav");
 const backdrop = document.querySelector(".backdrop");
 const container = document.querySelector(".container");
 const body = document.querySelector("body");
+
+// Menu
 
 // Creation de la classe open-nav quand la classe trigger est cliquer
 trigger.addEventListener("click", () => nav.classList.add("open-nav"));
@@ -16,6 +19,7 @@ backdrop.addEventListener("click", () =>
   container.classList.remove("nav_is_open")
 );
 
+// Écriture Accueil 
 class TypeWriter {
   constructor(txtElement, words, wait = 3000) {
     this.txtElement = txtElement;
@@ -70,6 +74,8 @@ class TypeWriter {
   }
 }
 
+// TabList
+
 // Init On DOM Load
 document.addEventListener("DOMContentLoaded", init);
 
@@ -80,4 +86,65 @@ function init() {
   const wait = txtElement.getAttribute("data-wait");
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll('[role="tab"]');
+  const tabList = document.querySelector('[role="tablist"]');
+
+  // Add a click event handler to each tab
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", changeTabs);
+  });
+
+  // Enable arrow navigation between tabs in the tab list
+  let tabFocus = 0;
+
+  tabList.addEventListener("keydown", (e) => {
+    // Move right
+    if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
+      tabs[tabFocus].setAttribute("tabindex", -1);
+      if (e.key === 'ArrowRight') {
+        tabFocus++;
+        // If we're at the end, go to the start
+        if (tabFocus >= tabs.length) {
+          tabFocus = 0;
+        }
+        // Move left
+      } else if (e.key === 'ArrowLeft') {
+        tabFocus--;
+        // If we're at the start, move to the end
+        if (tabFocus < 0) {
+          tabFocus = tabs.length - 1;
+        }
+      }
+
+      tabs[tabFocus].setAttribute("tabindex", 0);
+      tabs[tabFocus].focus();
+    }
+  });
+});
+
+function changeTabs(e) {
+  const target = e.target;
+  const parent = target.parentNode;
+  const grandparent = parent.parentNode;
+
+  // Remove all current selected tabs
+  parent
+    .querySelectorAll('[aria-selected="true"]')
+    .forEach((t) => t.setAttribute("aria-selected", false));
+
+  // Set this tab as selected
+  target.setAttribute("aria-selected", true);
+
+  // Hide all tab panels
+  grandparent
+    .querySelectorAll('[role="tabpanel"]')
+    .forEach((p) => p.setAttribute("hidden", true));
+
+  // Show the selected panel
+  grandparent.parentNode
+    .querySelector(`#${target.getAttribute("aria-controls")}`)
+    .removeAttribute("hidden");
 }
